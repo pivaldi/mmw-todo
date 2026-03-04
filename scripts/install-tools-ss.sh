85502dc314cd9a64d30349697b3b76a6cb062139
pivaldi <git@ivaldi.me>
2026-03-04 19:36:44 +0100

    Use stow to manage shared script/config

--- scripts/install-tools-ss.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-
-SCRIPT="$SCRIPT_DIR/install-tools.sh"
-STEPPS=$(grep -c 'st.done' "$SCRIPT")
-
-stream-stepper --wait=2 --processor=stbash --steps="$STEPPS" "$SCRIPT"
